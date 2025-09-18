88c3be3cc982b81eb66b687bdc9bebfcf755f4d4
Sandhiya120 <sandhiyass120499@gmail.com>
2025-09-18 11:47:33 +0530

    todaytask

--- git.js
+++ git.js
@@ -1,6 +1,2 @@
 console.log("Day one task completed");
-<<<<<<< HEAD
-console.log("Day three task completed")
-=======
-console.log("Day two task completed");
->>>>>>> 96c679c3c557d4145676388d8fb0783036d16d0e
+console.log("Day three task completed"):
