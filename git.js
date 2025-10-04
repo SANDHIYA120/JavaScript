8ea1b68e14c91e5b7272de14670b1026852d2917
Sandhiya120 <sandhiyass120499@gmail.com>
2025-10-04 15:09:21 +0530

    sadhiya updated

--- git.js
+++ git.js
@@ -1,3 +1,11 @@
 console.log("Day one task completed");
 console.log("Day three task completed");
 console.log("Day four task completed");
+
+
+
+
+
+// added by sandhiya
+
+console.log("sandhiya");
